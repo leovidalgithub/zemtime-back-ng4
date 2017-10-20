c7d65f3df9c684434a970900e0a4a2dcc1e4c0eb
Nicolas Balduzzi <nbalduzzi@zemsania.com>
2017-10-20 14:42:51 +0200

    swagger

--- src/config/config.js
+++ src/config/config.js
@@ -3,10 +3,23 @@ module.exports = {
   db: {
     url: `mongodb://192.168.16.40:27017/ia_zemtime_${process.env.NODE_ENV}`
   },
-  enail: {
+  email: {
     host: 'smtp.zemsania.com',
     port: 25,
     user: 'gitlab@zemsania.com',
     pass: 'K1Y32Q8bKMbaG6q'
+  },
+  documentation: {
+    swagger: {
+      info: {
+        title: 'Zemtime Backend Documentation',
+        description: 'testing the fastify swagger api',
+        version: '2.0.0'
+      },
+      host: 'localhost',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json']
+    }
   }
 }
--- src/endpoints/calendar-endpoint.js
+++ src/endpoints/calendar-endpoint.js
@@ -1,4 +1,5 @@
 const { getAll, create } = require('../handlers/calendar-handler')
+const schema = require('../schemas/calendar-schema')
 
 /**
  * Se agregan los handlers de cada endpoint del calendario.
@@ -7,8 +8,8 @@ const { getAll, create } = require('../handlers/calendar-handler')
  * @param {*} fastify
  */
 const endpoints = fastify => {
-  fastify.get('/calendars', (request, reply) => getAll(request, reply, fastify.mongo))
-  fastify.post('/calendar/:name/:type', (request, reply) => create(request, reply, fastify.mongo))
+  fastify.get('/calendars', schema.getAll, (request, reply) => getAll(request, reply, fastify.mongo))
+  fastify.post('/calendar/:name/:type', schema.create, (request, reply) => create(request, reply, fastify.mongo))
 }
 
 module.exports = endpoints
